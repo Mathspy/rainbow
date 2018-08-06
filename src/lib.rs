58f47c59331292911a555ab5c37815fe34339884
Mathspy <mathspy257@gmail.com>
2018-08-07 01:29:20 +0200

    First colour varation!

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
+pub enum Color {
+    RGB(u8, u8, u8)
+}
+
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn color_can_be_rgb() {
+        let rgb_color = Color::RGB(200, 100, 54);
+        let Color::RGB(r, g, b) = rgb_color;
+        assert_eq!(r, 200);
+        assert_eq!(g, 100);
+        assert_eq!(b, 54);
     }
 }
